3004d8b097051cde5237c764d0f83832c1b57db1
Jonathan Beliën <dev@jbelien.be>
2021-03-21 15:22:52 +0100

    Initial commit

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'relationId' => 62649,
+
+    'languages' => ['de', 'en'],
+
+    'exclude' => [
+        'relation' => [],
+        'way' => [],
+    ],
+
+    'gender' => [
+        'relation' => [],
+        'way' => [],
+    ],
+
+    'instances' => [
+        'Q5'        => true,  // human
+        'Q2985549'  => true,  // mononymous person
+        'Q20643955' => true,  // human biblical figure
+    ],
+];
